891f941e23cba9d6412a431e62f8a9008c2166db
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 20:47:34 -0300

    Backend: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // enable auto-validation, used for validating dto's
   app.useGlobalPipes(new ValidationPipe());
