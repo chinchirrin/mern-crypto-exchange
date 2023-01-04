b6ad1706c2b576da51bab2fff95b92b46cc4df22
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 18:33:37 -0300

    Backend: inject CronService as a provider of CryptoPricesModule and import CurrencyExchangeModule, so that we can call the endpoint for fetching and storing crypto live prices

--- backend/src/crypto-prices/crypto-prices.module.ts
+++ backend/src/crypto-prices/crypto-prices.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CurrencyExchangeModule } from 'src/currency-exchange/currency-exchange.module';
+import { CronService } from './cron.service';
 import { CryptoPricesController } from './crypto-prices.controller';
 import { CryptoPricesService } from './crypto-prices.service';
 
 @Module({
+  imports: [CurrencyExchangeModule],
   controllers: [CryptoPricesController],
-  providers: [CryptoPricesService],
+  providers: [CryptoPricesService, CronService],
 })
 export class CryptoPricesModule {}
