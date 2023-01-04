402893bfa7d866d579f7b67ce5b157b013070365
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 15:27:06 -0300

    Backed: enable mongoose package in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { CurrencyExchangeService } from './currency-exchange/currency-exchange.service';
 
@@ -8,6 +9,7 @@ import { CurrencyExchangeService } from './currency-exchange/currency-exchange.s
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
   ],
   controllers: [AppController],
   providers: [CurrencyExchangeService],
