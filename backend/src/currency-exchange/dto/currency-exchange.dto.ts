4b073266b2338777bad9814a6a4a355ce843a0ca
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 17:04:48 -0300

    Backed: add dto for currency-exchange records to be stored in the db

---
+++ backend/src/currency-exchange/dto/currency-exchange.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, IsNumberString } from 'class-validator';
+
+export class CurrencyExchangeDto {
+  @IsString() readonly currency_from: string;
+  @IsNumberString() readonly amount_from: number;
+  @IsString() readonly currency_to: string;
+  @IsNumberString() readonly amount_to: number;
+  @IsString() readonly type: string;
+}
