34c0f18cbda18e49f2388f8e9631fbdb7d5be0ab
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 17:42:01 -0300

    Backed: enable crypto prices api simulation module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { CurrencyExchangeModule } from './currency-exchange/currency-exchange.module';
+import { CryptoPricesModule } from './crypto-prices/crypto-prices.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { CurrencyExchangeModule } from './currency-exchange/currency-exchange.mo
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest'),
     CurrencyExchangeModule,
+    CryptoPricesModule,
   ],
   controllers: [AppController],
 })
