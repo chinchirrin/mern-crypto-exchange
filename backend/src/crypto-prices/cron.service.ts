1c8251e6111c366bdc7678ac97bfc5b9b9c88828
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 19:13:14 -0300

    Backend: add `CronService`, it performs the scheduled fetching and storing of live prices

---
+++ backend/src/crypto-prices/cron.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { CurrencyExchangeService } from 'src/currency-exchange/currency-exchange.service';
+
+@Injectable()
+export class CronService {
+  private readonly logger = new Logger(CronService.name);
+
+  constructor(
+    private readonly cryptoLivePricesService: CurrencyExchangeService,
+  ) {}
+
+  /**
+   * Fetch crypto live prices every five minutes
+   */
+  @Interval('livepricesupdates', 1000 * 60 * 5)
+  scheduledLivePrices() {
+    this.cryptoLivePricesService.createRates();
+    this.logger.log('Crypto live prices were fetched and saved!');
+  }
+}
