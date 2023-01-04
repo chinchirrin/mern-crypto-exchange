cf3b24d6da6e6e4f8f7f9eeae7003e74d767edfa
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 17:43:33 -0300

    Backend: enable validation pipes for dto's

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable auto-validation, used for validating dto's
+  app.useGlobalPipes(new ValidationPipe());
 
   // load configs from .env file
   const configService = app.get(ConfigService);
