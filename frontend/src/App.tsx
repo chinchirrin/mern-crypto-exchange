775b317cc324bd4d9da639b38efc0a56aaf35f78
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-04 20:38:20 -0300

    Frontend: update app to display input form and data grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {HistoricalTable} from './components/historicaltable';
+import {Toolbar} from './components/toolbar';
+import {socket, WebSocketProvider} from './context/websocketcontext';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <WebSocketProvider value={socket}>
+        <Toolbar />
+        <HistoricalTable />
+      </WebSocketProvider>
     </div>
   );
 }
