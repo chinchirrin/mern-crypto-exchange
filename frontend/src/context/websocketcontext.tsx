dac06bcd940758a98b8c354cf7c3356ee6e6ee22
Andres Rojas Moran <arojasmoran@gmail.com>
2023-01-05 02:01:16 -0300

    Frontend: fix typo when provisioning websocket url from environment

--- frontend/src/context/websocketcontext.tsx
+++ frontend/src/context/websocketcontext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const wsUrl = process.env.WS_URL || '';
+const wsUrl = process.env.REACT_APP_WS_URL || '';
 export const socket = io(wsUrl);
 export const WebSocketContext = createContext<Socket>(socket);
 export const WebSocketProvider = WebSocketContext.Provider;
